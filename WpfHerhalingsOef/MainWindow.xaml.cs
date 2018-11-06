62fca61a3bcc81dc4792dd3c65ae7db01ee6ba69
Kenenth Buyck <kennethbuyck@gmail.com>
2018-11-06 15:58:07 +0100

    Oef 2 klaar

--- WpfHerhalingsOef/MainWindow.xaml.cs
+++ WpfHerhalingsOef/MainWindow.xaml.cs
@@ -72,5 +72,79 @@ namespace WpfHerhalingsOef
             // Maak de listbox leeg
             lstGegevens.Items.Clear();
         }
+
+        private void btnOef2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ingave = int.Parse(txtIngave.Text);
+
+                if (ingave < 1 || ingave > 10)
+                {
+                    throw new IndexOutOfRangeException("Kies een getal tussen 1 en 10");
+                }
+                else
+                {
+                    switch (ingave)
+                    {
+                        case 1:
+                            lstGegevens.Items.Add("1 : één");
+                            break;
+                        case 2:
+                            lstGegevens.Items.Add("2 : Twee");
+                            break;
+                        case 3:
+                            lstGegevens.Items.Add("3 : Drie");
+                            break;
+                        case 4:
+                            lstGegevens.Items.Add("4 : Vier");
+                            break;
+                        case 5:
+                            lstGegevens.Items.Add("5 : Vijf");
+                            break;
+                        case 6:
+                            lstGegevens.Items.Add("6 : Zes");
+                            break;
+                        case 7:
+                            lstGegevens.Items.Add("7 : Zeven");
+                            break;
+                        case 8:
+                            lstGegevens.Items.Add("8 : Acht");
+                            break;
+                        case 9:
+                            lstGegevens.Items.Add("9 : Negen");
+                            break;
+                        case 10:
+                            lstGegevens.Items.Add("10 : Tien");
+                            break;
+                    }
+                }
+            }
+            catch (FormatException fEx)
+            {
+                MessageBox.Show("Gelieve enkel gehele getallen in te voeren!\n\n"
+                    + "Details: " + fEx.Message,
+                    "Fout!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oops.. er is een fout gebeurd\n\n" +
+                    "Details: " + ex.Message,
+                    "Fout!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+            finally
+            {
+                Debug.WriteLine("Laatste uitvoering: {0:dd/MM/yyyy HH:mm:ss}", DateTime.Now);
+            }
+        }
+
+        private void btnOef3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
